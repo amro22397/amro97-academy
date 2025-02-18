1136d102f4f5e813d7b4318c6bf1aa587963e76f
amro22397 <amroalmutasim22@gmail.com>
2025-02-18 17:23:48 +0400

    get user login

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -1,17 +1,17 @@
-import { getSession } from "@/actions/getUser";
+import { getSession, getUser } from "@/actions/getUser";
 import { LoginForm } from "@/components/login-form"
 import { redirect } from "next/navigation";
 
 export default async function Page() {
 
-  // const session = await getSession();
+  const session = await getUser();
   
-  //   // console.log(session);
+    console.log(session);
   
   
-  //   //   if (session?.user?.email) {
-  //   //     redirect('/');
-  //   //   }
+      if (session?.user?.email) {
+        redirect('/');
+      }
 
   return (
     <div className="flex w-full items-center justify-center p-6 md:p-10
